aa182fd0f3572078043413e3a674cd2c8e7220cc
ziad khaled <zikokhaled50@gmail.com>
2025-07-31 09:15:39 +0300

    Reorganized repo to track entire NZWalks solution

---
+++ NZWalks.API/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace NZWalks.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly ILogger<ExceptionHandlerMiddleware> logger;
+        private readonly RequestDelegate next;
+
+        public ExceptionHandlerMiddleware(ILogger<ExceptionHandlerMiddleware> logger, RequestDelegate next)
+        {
+            this.logger = logger;
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+
+            try
+            {
+                await next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                var errorId = Guid.NewGuid();
+
+                // Log this exception
+                logger.LogError(ex, $"{errorId} : {ex.Message}");
+
+                // Return a custom error response
+                httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                httpContext.Response.ContentType = "application/json";
+
+                var error = new
+                {
+                    Id = errorId,
+                    ErrorMessage = "Something went wrong!"
+                };
+
+                await httpContext.Response.WriteAsJsonAsync(error);
+
+            }
+
+        } 
+
+    }
+}
